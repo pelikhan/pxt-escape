8688b401dc62f3bb586832e7370da130e6bb9f89
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-12-13 12:20:54 -0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -3,6 +3,7 @@ namespace escape {
     export const RADIO_GROUP = 42
     export const ADD_MINUTE = 1
     export const REMOVE_MINUTE = 2
+    export const RESET_CLOCK = 3
 
     function init() {
         radio.setGroup(RADIO_GROUP);
