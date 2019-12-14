01d1b94a5be41e3c8bb2a8e9a2d83e89e4c7d9b7
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-12-13 17:23:12 -0800

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,5 +1,6 @@
 // tests go here; this will not be compiled when this package is used as an extension.
 basic.showString("ESCAPE")
+radio.setTransmitPower(7)
 radio.onReceivedBuffer(b => {
     led.toggle(0, 0)
     escape.logMessage(b);
